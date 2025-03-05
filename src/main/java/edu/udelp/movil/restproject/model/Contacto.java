7c8b55e5215ea03f5ee110e875fd508551a70004
Luis Raúl <luisraulreyes@gmail.com>
2025-03-04 23:46:56 -0600

    Update Contacto.java
    Eliminacion de pattern y regex

--- src/main/java/edu/udelp/movil/restproject/model/Contacto.java
+++ src/main/java/edu/udelp/movil/restproject/model/Contacto.java
@@ -24,22 +24,15 @@ public class Contacto {
 	
 	@NotNull(message = "Campo Obligatorio") //No deja que esta columna reciba datos nulos
 	@Column(name = "fechaNacimiento")
-	private String fechaNacimiento;
+	private LocalDate fechaNacimiento;
 	
 	@NotNull(message = "Campo Obligatorio")
     @NotEmpty(message = "Campo Obligatorio")
-	@Pattern(regexp = "^[a-zA-Z0-9._%+-]+@(gmail\\.com|outlook\\.com)$",
-		    message = "El correo debe ser @gmail.com o @outlook.com") //Acepta letra y simbolos que deja el correo y tiene que terminar en @gmail.com
-	@Column(length = 100)
 	private String email;
 
 	@NotNull(message = "Campo Obligatorio")
     @NotEmpty(message = "Campo Obligatorio")
 	@Column(length = 10)
-	@Pattern(
-	        regexp = "^[0-9]{10}$", //Unicamente numeros y {10} que nomás puede tener 10 digitos no mas
-	        message = "El número de teléfono debe tener exactamente 10 dígitos"
-	    )
 	private String numeroTelefono;
 
 	private String foto;  //Se pone String porque se va a poner la ruta de la foto
